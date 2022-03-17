c234a3625a9b9180a7ca93e7e995729284fef5a8
SreyankHub <96630625+SreyankHub@users.noreply.github.com>
2022-03-17 00:33:43 -0700

    Add files via upload

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> FavoriteFood = new ArrayList<String>();
+
+        FavoriteFood.add("IceCream");
+        FavoriteFood.add("Pizza");
+        //FavoriteFood.remove(0);
+
+        System.out.println(FavoriteFood);
+
+    }
+}
